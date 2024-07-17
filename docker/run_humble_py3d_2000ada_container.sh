21048eda904753cc3cbf42df839ba87151ccb3a5
Amedeo Fabris <amedeo.fba@gmail.com>
2024-07-17 11:25:18 +0200

    Add new docker image for new pc with new GPU

--- docker/run_humble_py3d_2000ada_container.sh
+++ docker/run_humble_py3d_2000ada_container.sh
@@ -8,11 +8,11 @@ xhost +
 
 # Running container
 docker run -it --privileged --rm \
-               --name humble_py3d_mx130 \
+               --name humble_py3d_2000ada \
                --net=host \
                --gpus all \
                -e DISPLAY=$DISPLAY \
                -v /tmp/.X11-unix:/tmp/.X11-unix \
                -v $SCRIPT_DIR/../../units_ws:/home/code \
                -w /home/code \
-               humble_py3d_mx130 bash
+               humble_py3d_2000ada bash
