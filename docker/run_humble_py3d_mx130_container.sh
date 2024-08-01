a6912f3fa8e2173b5d04c5b8558574dc35d0f3e4
Amedeo Fabris <amedeo.fba@gmail.com>
2024-08-01 09:50:55 +0200

    Accessing to gui display from docker via Xauthority

--- docker/run_humble_py3d_mx130_container.sh
+++ docker/run_humble_py3d_mx130_container.sh
@@ -3,9 +3,6 @@
 # Get the full path to itself
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# Giving permession to forward GUIs streams
-xhost +
-
 # Running container
 docker run -it --privileged --rm \
                --name humble_py3d_mx130 \
@@ -13,6 +10,7 @@ docker run -it --privileged --rm \
                --gpus all \
                -e DISPLAY=$DISPLAY \
                -v /tmp/.X11-unix:/tmp/.X11-unix \
+               -v $XAUTHORITY:/dot.Xauthority \
                -v $SCRIPT_DIR/../../units_ws:/home/code \
                -w /home/code \
                humble_py3d_mx130 bash
