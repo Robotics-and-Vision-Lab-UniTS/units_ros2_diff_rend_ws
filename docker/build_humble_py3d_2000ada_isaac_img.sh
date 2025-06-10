b7e1de185faabf50b0639dde9b3d0a3e230d0839
Amedeo Fabris <amedeo.fba@gmail.com>
2025-06-10 10:30:36 +0200

    Adding working docker container for isaac sim 4.2.0

---
+++ docker/build_humble_py3d_2000ada_isaac_img.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t humble_py3d_2000ada_isaac -f humble_py3d_2000ada_isaac/Dockerfile .
---
+++ docker/run_humble_py3d_2000ada_isaac_container.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+xhost +local:root
+
+# Get the full path to itself
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Running container
+docker run -it --privileged --rm \
+               --name humble_py3d_2000ada_isaac \
+               --net=host \
+               --gpus all \
+               -e DISPLAY=$DISPLAY \
+               -e OMNI_ACCEPT_EULA=YES \
+               -e OMNI_ENV_PRIVACY_CONSENT=YES \
+               -e OMNI_KIT_ALLOW_ROOT=1 \
+               -e XDG_RUNTIME_DIR=/run/user/$(id -u) \
+               -v /tmp/.X11-unix:/tmp/.X11-unix \
+               -v $XAUTHORITY:/root/.Xauthority \
+               -v $SCRIPT_DIR/../../units_ws:/home/code \
+               -w /home/code \
+               --entrypoint /bin/bash \
+               humble_py3d_2000ada_isaac
