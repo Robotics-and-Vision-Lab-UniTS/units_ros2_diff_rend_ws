49436a3c6ba3d3cf1d8bc4acc3b3df2ba8a07fcb
Erica <42058580+EricaSalvato@users.noreply.github.com>
2025-07-14 09:53:08 +0200

    Working docker container on windows on WSL2

--- docker/run_humble_py3d_quadro_p2200_container.sh
+++ docker/run_humble_py3d_quadro_p2200_container.sh
@@ -8,8 +8,10 @@ docker run -it --privileged --rm \
                --name humble_py3d_quadro_p2200 \
                --net=host \
                --gpus all \
-               -e DISPLAY=host.docker.internal:0.0 \
+               -e DISPLAY=$DISPLAY \
                -e QT_X11_NO_MITSHM=1 \
+               -v /tmp/.X11-unix:/tmp/.X11-unix \
+               -v $HOME/.Xauthority:/root/.Xauthority:rw \
                -v $SCRIPT_DIR/../../units_ws:/home/code \
                -v /dev/snd:/dev/snd:ro \
                -w /home/code \
